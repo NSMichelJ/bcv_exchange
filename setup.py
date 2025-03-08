e084f9e06c7cebe1c789104fcadbf220ef1c0f71
NSMichelJ <nunezsalmeronm@gmail.com>
2025-03-08 12:12:22 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
 DESCRIPTION = 'Unofficial Python package to retrieve official exchange rates from the Central Bank of Venezuela (BCV) website.'
 PACKAGE_NAME = 'bcv_exchange'
 AUTHOR = 'NSMichelJ'
